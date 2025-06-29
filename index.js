0035b41df29422d60dc640adfff28ada7f13eb65
BRIAN GITHINJI <brian.githinji3@student.moringaschool.com>
2025-06-29 01:28:21 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(saturdayFun = "roller-skate") {
+  return `This Saturday, I want to ${saturdayFun}!`;
+}
+
+function mondayWork(mondayWork = "go to the office") {
+  return `This Monday, I will ${mondayWork}.`;
+}
+
+function wrapAdjective(wrapper = "*") {
+  return function(adjective) {
+    return `You are ${wrapper}${adjective}${wrapper}!`;
+  };
+}
